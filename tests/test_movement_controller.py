886b4925e41dd6d23f20cf868266d914bd922134
longzdez-debug <longz.dez@gmail.com>
2026-09-21 12:05:21 +0300

    test: cover WalkBot input and steering

--- tests/test_movement_controller.py
+++ tests/test_movement_controller.py
@@ -10,3 +10,26 @@ def test_movement_controller_is_the_only_adapter_boundary_for_intent():
     assert adapter.last == (True, False, True, False)
     controller.apply(MovementIntent(stop=True))
     assert adapter.last == (False, False, False, False)
+
+
+def test_movement_controller_forwards_turn_when_adapter_supports_it():
+    from velora.movement_controller import MovementController
+    from velora.movement_intent import MovementIntent
+
+    class Adapter:
+        def __init__(self):
+            self.moves = []
+            self.turns = []
+
+        def move(self, forward, back, left, right):
+            self.moves.append((forward, back, left, right))
+
+        def release_all(self):
+            self.moves.append(("release",))
+
+        def turn(self, amount):
+            self.turns.append(amount)
+
+    adapter = Adapter()
+    MovementController(adapter).apply(MovementIntent(forward=1, turn=0.5))
+    assert adapter.turns == [0.5]
