6994736f22c76f03f6af22da32909d140a770a3b
longzdez-debug <longz.dez@gmail.com>
2026-09-21 05:48:00 +0300

    Test safe process claiming

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,4 +1,7 @@
 from velora.process import ProcessSupervisor
 
-def test_process_supervisor_starts_empty():
- assert ProcessSupervisor().owned=={}
+def test_empty_supervisor():
+    assert ProcessSupervisor().owned == {}
+
+def test_claim_missing_process_is_safe():
+    assert ProcessSupervisor().claim(999999999, "C:/missing/cs2.exe") is None
