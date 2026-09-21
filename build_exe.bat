165a36af5518199acc3fdbb996b263927b01b08a
longzdez-debug <longz.dez@gmail.com>
2026-09-21 11:14:46 +0300

    Build: expose pytest diagnostics and detect hangs

--- build_exe.bat
+++ build_exe.bat
@@ -31,8 +31,10 @@ if errorlevel 1 goto :error
 if errorlevel 1 goto :error
 
 echo [4/7] Running tests...
-".venv\Scripts\python.exe" -m pytest
-if errorlevel 1 goto :tests_error
+".venv\Scripts\python.exe" -m pytest -vv -ra -o faulthandler_timeout=30 > "pytest-build.log" 2>&1
+set "TEST_EXIT=%ERRORLEVEL%"
+type "pytest-build.log"
+if not "%TEST_EXIT%"=="0" goto :tests_error
 
 echo [5/7] Installing PyInstaller...
 ".venv\Scripts\python.exe" -m pip install --upgrade pyinstaller
